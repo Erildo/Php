742d217ceef0663beb4a255f13519b40d66cea0c
Erildo Shuli <Erildo@users.noreply.github.com>
2016-08-02 14:12:15 +0200

    Add files via upload

---
+++ 2/view.php
@@ -0,0 +1,49 @@
+<?php
+include_once 'dbconfig.php';
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>File Uploading With PHP and MySql</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<div id="header">
+<label>Your Documents</label>
+</div>
+<div id="body">
+	<table width="80%" border="1">
+    <tr>
+    <th colspan="4">Upload your files <label>
+	<a href="index.php">Add new</a></label>
+	</br>Add Documents:</br>including Cvs,Code,Letters or Images
+	
+	</th>
+    
+	</tr>
+    <tr>
+    <td>File Name</td>
+    <td>File Type</td>
+    <td>File Size(KB)</td>
+    <td>View</td>
+    </tr>
+    <?php
+	$sql="SELECT * FROM tbl_uploads";
+	$result_set=mysqli_query($connection,$sql);
+	while($row=mysqli_fetch_array($result_set))
+	{
+		?>
+        <tr>
+        <td><?php echo $row['file'] ?></td>
+        <td><?php echo $row['type'] ?></td>
+        <td><?php echo $row['size'] ?></td>
+        <td><a href="uploads/<?php echo $row['file'] ?>" target="_blank">view file</a></td>
+        </tr>
+        <?php
+	}
+	?>
+    </table>
+    
+</div>
+</body>
+</html>
